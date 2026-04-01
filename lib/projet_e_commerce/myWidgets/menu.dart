4f66c4e97ad469c0e3c047fb60edac0ff242c8d4
azizkarouii <med.aziz.el.karoui@gmail.com>
2026-04-01 10:27:53 +0100

    01/04/26 v2

--- lib/projet_e_commerce/myWidgets/menu.dart
+++ lib/projet_e_commerce/myWidgets/menu.dart
@@ -5,8 +5,8 @@ class Menu extends StatefulWidget {
 
   @override
   State<Menu> createState() => _MyWidgetSpace();
-
 }
+
 class _MyWidgetSpace extends State<Menu> {
   @override
   Widget build(BuildContext context) {
@@ -14,13 +14,49 @@ class _MyWidgetSpace extends State<Menu> {
       child: ListView(
         children: [
           UserAccountsDrawerHeader(
-          accountName: Text("Mohamed Aziz Karoui"),
-          accountEmail: Text("mohamed.aziz.karoui@gmail.com"),
-          currentAccountPicture: CircleAvatar(
-            backgroundImage: NetworkImage(
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnSA1zygA3rubv-VK0DrVcQ02Po79kJhXo_A&s"),
+            accountName: Text("Mohamed Aziz Karoui"),
+            accountEmail: Text("mohamed.aziz.karoui@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnSA1zygA3rubv-VK0DrVcQ02Po79kJhXo_A&s",
+              ),
+            ),
           ),
-        )
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text("Accueil"),
+            onTap: () {
+              Navigator.pushNamed(context, "home");
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.list_alt),
+            title: Text("Liste Produit"),
+            onTap: () {
+              Navigator.pushNamed(context, "ListeProduits");
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_cart),
+            title: Text("Panier"),
+            onTap: () {
+              Navigator.pushNamed(context, "PanierPage");
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text("Mes Favoris"),
+            onTap: () {
+              Navigator.pushNamed(context, "FavoritePage");
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Profil"),
+            onTap: () {
+              Navigator.pushNamed(context, "UserInfoPage");
+            },
+          ),
         ],
       ),
     );
